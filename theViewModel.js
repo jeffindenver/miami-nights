c4eb8f3423ef628e43d08af1272f3b32fd9e68a8
Jeff Shepherd <jeffindenver2002@yahoo.com>
2020-03-15 18:23:42 -0500

    added theModel class and unitTests

---
+++ theViewModel.js
@@ -0,0 +1,17 @@
+/*
+===============================================================================
+; Title:  theModel.js
+; Author: Jeff Shepherd, Janet Blohn
+; Modified By:
+; Date:   3/14/2020
+; Description: The ViewModel for the JavaScript Quiz.
+; The ViewModel should handle data and behavior. It applies knockout observables
+; (data bindings), exposes functions, takes input from view and places on model
+; and vice versa, triggers events on view, and manipulates the model.
+;==============================================================================
+*/
+
+"use strict";
+
+const theModel = require("./theModel.js");
+const quiz = theModel.getData();
