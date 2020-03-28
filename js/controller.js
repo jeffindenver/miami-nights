d12513e39c0038af595db84a30ea0fecaed5b2d7
Jeff Shepherd <jeffindenver2002@yahoo.com>
2020-03-28 16:39:25 -0500

    added retry button. fixed styles

--- js/controller.js
+++ js/controller.js
@@ -8,13 +8,21 @@ $(function () {
 
   $("#begin").click(() => {
     $(".instructions").hide(400);
-    $("#begin").fadeOut(200);
+    $("#begin").hide();
+    $("#submit").fadeIn(1000);
     $("#myCarousel").css("display", "block");
     $("#question1").addClass("active");
-
   });
 
   $("#submit").click(function () {
+    $("#myCarousel").hide(400);
+    $(".summary").show(400);
+    $("#submit").fadeOut(200);
+    $("#retry").fadeIn(1000);
     viewModel.report();
+  });
+
+  $("#retry").click( () => {
+    location.reload();
   });
 });
